3f08cfbaa1aae51eb9efed261a8660e6637e263e
OutcomesSourced <43861437+OutcomesSourced@users.noreply.github.com>
2018-10-04 22:05:46 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -26,7 +26,7 @@
 
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
-          var gender = patient.identifier
+          var gender = patient.identifier;
           var phone = patient.gender; 
 
           var fname = '';
