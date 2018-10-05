a9967373fb2214b0d961e43fe9c997a7db94b36d
OutcomesSourced <43861437+OutcomesSourced@users.noreply.github.com>
2018-10-05 14:06:21 -0500

    Test28

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -33,7 +33,7 @@
       var row = table.insertRow(1);
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
-      cell1.innerHTML = condition.
+      cell1.innerHTML = '1';
       cell2.innerHTML = '2';
       
       //if (typeof observation.code.coding[0].code != 'undefined' &&
@@ -119,7 +119,7 @@
             lname = patient.name[0].family.join(' ');
           }
 
-          fname = "Test27";
+          fname = "Test28";
           var height = byCodes('8302-2');
           var weight = byCodes('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
