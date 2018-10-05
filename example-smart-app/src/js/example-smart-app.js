17a16a7d3fbe32300904699bb8add39af1a94f79
OutcomesSourced <43861437+OutcomesSourced@users.noreply.github.com>
2018-10-04 21:47:25 -0500

    Adding Phone Attempt 7

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -27,7 +27,7 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
-          var phone = Patient.telecom.where(system='phone');
+          var phone = patient.telecom.where(system='phone');
 
           var fname = '';
           var lname = '';
