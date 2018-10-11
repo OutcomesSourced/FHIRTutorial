8d70e5e7f4c9387d751b2c02b694bca1d28d9a7e
OutcomesSourced <43861437+OutcomesSourced@users.noreply.github.com>
2018-10-11 12:31:07 -0500

    Read()? - 46

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -112,7 +112,7 @@ let csvContent = "data:text/csv;charset=utf-8,";
    csvContent += row + "\r\n";
 }); 
 */
-        csvContent += obv.outerHTML;
+        csvContent += obv.read();
         
 
 var encodedUri = encodeURI(csvContent);
@@ -139,7 +139,7 @@ window.open(encodedUri);
             lname = patient.name[0].family.join(' ');
           }
 
-          fname = "Test45";
+          fname = "Test46";
           var height = byCodes('8302-2');
           var weight = byCodes('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
