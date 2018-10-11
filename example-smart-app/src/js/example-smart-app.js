42a6419ca4a3aeac7a1053e5d6fc385e11c5388f
OutcomesSourced <43861437+OutcomesSourced@users.noreply.github.com>
2018-10-11 12:09:34 -0500

    Adding a download

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -103,6 +103,19 @@
           });
         });
         
+        
+        const rows = [["name1", "city1", "some other info"], ["name2", "city2", "more info"]];
+let csvContent = "data:text/csv;charset=utf-8,";
+rows.forEach(function(rowArray){
+   let row = rowArray.join(",");
+   csvContent += row + "\r\n";
+}); 
+
+
+var encodedUri = encodeURI(csvContent);
+window.open(encodedUri);
+        
+        
         // display in alert box
         //alert(xml_to_string(smart.patient.api.fetchAll({type: 'Observation'})));
         
