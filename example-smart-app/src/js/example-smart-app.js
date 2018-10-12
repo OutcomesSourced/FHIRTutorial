24883dfef652dc3b3e2139881fc93445981f8836
OutcomesSourced <43861437+OutcomesSourced@users.noreply.github.com>
2018-10-12 12:41:52 -0500

    Don;t make files - 94

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -169,6 +169,7 @@ function createFHIRFile(resource){
                                  //"Schedule",
                                  //"Slot"
                                  ], len = resources.length;
+         /*
           for (i=0; i<len; ++i) {
             if (i in resources) {
               smart.patient.api.fetchAll({type: resources[i]})
@@ -180,6 +181,7 @@ function createFHIRFile(resource){
               });
             }
           }
+      */
       
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
@@ -199,7 +201,7 @@ function createFHIRFile(resource){
   
 
 
-          fname = "Test93";
+          fname = "Test94";
           //fname = JSON.stringfy(patient);
           var height = byCodes('8302-2');
           var weight = byCodes('29463-7');
