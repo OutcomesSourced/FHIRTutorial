776bf6ecd4f105d775de18432a3c68cd335c3ad4
OutcomesSourced <43861437+OutcomesSourced@users.noreply.github.com>
2018-10-12 08:51:46 -0500

    79

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -120,7 +120,7 @@ function createFHIRFile(resource){
           
         
           
-          var resources = ["Observation","Condition"];        
+          //var resources = ["Observation","Condition"];        
           /*
           var testw = "Observation"
           
@@ -162,7 +162,7 @@ function createFHIRFile(resource){
   
 
 
-          fname = "Test78";
+          fname = "Test79";
           //fname = JSON.stringfy(patient);
           var height = byCodes('8302-2');
           var weight = byCodes('29463-7');
