97b98428e7dde9dffa1c2c91cb6f7fc4798cd7de
OutcomesSourced <43861437+OutcomesSourced@users.noreply.github.com>
2018-10-12 09:30:30 -0500

    adding paren - 82

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -62,7 +62,7 @@ function createFHIRFile(resource){
         var encodedUri = encodeURI(fileContent);
           var link = document.createElement("a");
           link.setAttribute("href", encodedUri);
-          if typeof resource.patient != 'undefined' {
+          if (typeof resource.patient != 'undefined') {
             link.setAttribute("download", resource.patient.reference.replace("Patient/","") + "_" + resource.resourceType + "_" + resource.id + ".fhir");    
           else { 
             link.setAttribute("download", resource.subject.reference.replace("Patient/","") + "_" + resource.resourceType + "_" + resource.id + ".fhir");
@@ -168,7 +168,7 @@ function createFHIRFile(resource){
   
 
 
-          fname = "Test81";
+          fname = "Test82";
           //fname = JSON.stringfy(patient);
           var height = byCodes('8302-2');
           var weight = byCodes('29463-7');
